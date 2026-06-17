7a96b2fc4cad8c22183d621cd084542933684dff
raghav-000 <rajpr071220@gamil.com>
2026-06-17 14:37:59 +0530

    use .ready

--- components/gsap/Copy.tsx
+++ components/gsap/Copy.tsx
@@ -24,68 +24,76 @@ const Copy: React.FC<React.PropsWithChildren<CopyProps>> = ({
     const lines = useRef<HTMLElement[]>([]);
 
     useGSAP(() => {
-        if (!containerRef.current) return;
 
-        splitRef.current = [];
-        elementRef.current = [];
-        lines.current = [];
+        const init = async () => {
+            if (!containerRef.current) return;
 
-        let elements: HTMLElement[] = [];
+            await document.fonts.ready;
 
-        if (containerRef.current.hasAttribute("data-copy-wrapper")) {
-            elements = Array.from(containerRef.current.children).filter(
-                (el): el is HTMLElement => el instanceof HTMLElement
-            );
-        } else {
-            elements = [containerRef.current];
-        }
+            splitRef.current = [];
+            elementRef.current = [];
+            lines.current = [];
 
-        elements.forEach((element) => {
-            elementRef.current.push(element);
+            let elements: HTMLElement[] = [];
 
-            const split = SplitText.create(element, {
-                type: "lines",
-                mask: "lines",
-                linesClass: "line++",
-            });
-
-            splitRef.current.push(split);
-
-            const computedStyle = window.getComputedStyle(element);
-            const textIndent = computedStyle.textIndent;
-
-            if (textIndent && textIndent !== "0px") {
-                if (split.lines.length > 0 && split.lines[0] instanceof HTMLElement) {
-                    split.lines[0].style.paddingLeft = textIndent;
-                }
-                element.style.textIndent = "0";
+            if (containerRef.current.hasAttribute("data-copy-wrapper")) {
+                elements = Array.from(containerRef.current.children).filter(
+                    (el): el is HTMLElement => el instanceof HTMLElement
+                );
+            } else {
+                elements = [containerRef.current];
             }
 
-            lines.current.push(...split.lines.filter((el): el is HTMLElement => el instanceof HTMLElement));
-        });
+            elements.forEach((element) => {
+                elementRef.current.push(element);
 
-        gsap.set(lines.current, { y: "100%" });
+                const split = SplitText.create(element, {
+                    type: "lines",
+                    mask: "lines",
+                    linesClass: "line++",
+                });
 
-        const animationProps = {
-            y: "0%",
-            duration: 0.4,
-            stagger: 0.1,
-            ease: "power4.out",
-            delay: delay,
+                splitRef.current.push(split);
+
+                const computedStyle = window.getComputedStyle(element);
+                const textIndent = computedStyle.textIndent;
+
+                if (textIndent && textIndent !== "0px") {
+                    if (split.lines.length > 0 && split.lines[0] instanceof HTMLElement) {
+                        split.lines[0].style.paddingLeft = textIndent;
+                    }
+                    element.style.textIndent = "0";
+                }
+
+                lines.current.push(...split.lines.filter((el): el is HTMLElement => el instanceof HTMLElement));
+            });
+
+            gsap.set(lines.current, { y: "100%" });
+
+            const animationProps = {
+                y: "0%",
+                duration: 0.4,
+                stagger: 0.1,
+                ease: "power4.out",
+                delay: delay,
+            };
+
+            if (animateOnScroll) {
+                gsap.to(lines.current, {
+                    ...animationProps,
+                    scrollTrigger: {
+                        trigger: containerRef.current,
+                        start: "top 75%",
+                        toggleActions: "play none none reset",
+                    }
+                });
+            } else {
+                gsap.to(lines.current, animationProps);
+            }
+
         };
 
-        if (animateOnScroll) {
-            gsap.to(lines.current, {
-                ...animationProps,
-                scrollTrigger: {
-                    trigger: containerRef.current,
-                    start: "top 75%",
-                    toggleActions: "play none none reset",
-                }
-            });
-        } else {
-            gsap.to(lines.current, animationProps);
-        }
+        init();
 
         return () => {
             splitRef.current.forEach((split) => {
